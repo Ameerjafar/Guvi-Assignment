3774783a31d047bef8f1b20b5b5509ffd7cb5979
Ameer jafar <ameerjafar123@gmail.com>
2024-03-20 15:46:20 +0530

    login code added

---
+++ js/login.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#loginForm').submit(function(e){
+        e.preventDefault(); 
+        var username = $('#username').val(); 
+        var password = $('#password').val(); 
+        $.ajax({
+            method: 'POST',
+            url: './php/login.php',
+            data: {
+                username: username,
+                password: password
+            },
+            success: function(response){
+                if(response === 'success') {
+                    alert("Login successful!");
+                    windows.location.href = 'profile.html';
+                } else {
+                    $('#message').html('Invalid username or password.');
+                }
+            },
+            error: function(xhr, status, error){
+                console.error(xhr.responseText);
+                alert('An error occurred while processing your request. Please try again.');
+            }
+        });
+    });
+});
